c57b55cfd55aab68ddfb4d94fa07ff47a7dd9980
DaanTacoSchool <daanve.zakelijk@gmail.com>
2018-06-06 14:58:28 +0200

    Improved delay method
    Fixed bugs with the decay timer
    radials start with 50  and then fast decay to 25 from there normal code resumes.

--- SeriousGame/Assets/Modern UI Pack/Scripts/ProgressBar.cs
+++ SeriousGame/Assets/Modern UI Pack/Scripts/ProgressBar.cs
@@ -13,11 +13,14 @@ public class ProgressBar : MonoBehaviour {
 	public bool restart;
 	[Range(0, 100)] public float currentPercent;
 	[Range(0, 100)] public int speed;
+    [Range(0, 10)] public float decayPerTick;
 
     [Header("SPECIFIED PERCENT")]
     public bool enableSpecified;
     public bool enableLoop;
     [Range(0, 100)] public float specifiedValue;
+
+    private bool isFilling;
 
     void Update ()
 	{
@@ -33,21 +36,38 @@ public class ProgressBar : MonoBehaviour {
                 currentPercent += speed * Time.deltaTime;
             }
 
+            //when in loop mode simply set current% to 0 else invert default update
             if (enableLoop == true && currentPercent >= specifiedValue)
             {
                 currentPercent = 0;
+            }else
+            if(currentPercent >= specifiedValue)
+            {
+                currentPercent -= speed * Time.deltaTime;
             }
+
         }
 
+        // loop
         if (currentPercent == 100 || currentPercent >= 100 && restart == true) 
 		{
 			currentPercent = 0;
 		}
+
+        // when above 1% and not filling up then decay
+        if (currentPercent >=1 && specifiedValue >=1 && isOn && isFilling==false)
+        {
+            specifiedValue -= (float)(decayPerTick/1000);
+           // currentPercent -= decayPerTick;
+        }
+
+        //TODO: verify that this also works when the bar is emptying
 		loadingBar.GetComponent<Image> ().fillAmount = currentPercent / 100;
 		textPercent.GetComponent<Text> ().text = ((int)currentPercent).ToString ("F0") + "%";
 	}
     public void AddToSpecifiedPercentage(float amountToAdd)
     {
+        isFilling = true;
         if(this.specifiedValue+amountToAdd > 100)
         {
             specifiedValue = 100;
@@ -56,6 +76,6 @@ public class ProgressBar : MonoBehaviour {
         {
             specifiedValue += amountToAdd;
         }
-        
+        isFilling = false;
     }
 }
