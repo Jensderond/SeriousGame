1d260d83f0ccc9fdb2368ba89b6b23b8b0e1d991
Koen de Bont <koen-debont@hotmail.com>
2018-06-13 18:07:03 +0200

    Shop UI

---
+++ SeriousGame/Assets/SampleButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+
+    public Button buttonComponent;
+    public Text nameLabel;
+    public Image iconImage;
+    public Text priceText;
+
+
+    private Item item;
+    private ShopScrollList scrollList;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonComponent.onClick.AddListener(HandleClick);
+    }
+
+    public void Setup(Item currentItem, ShopScrollList currentScrollList)
+    {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        iconImage.sprite = item.icon;
+        priceText.text = item.price.ToString();
+        scrollList = currentScrollList;
+
+    }
+
+    public void HandleClick()
+    {
+        scrollList.TryTransferItemToOtherShop(item);
+    }
+}
